621d39cc5f1a5ed52effc3f27f2101e8f062c541
stoodfarback <stoodfarback@gmail.com>
2020-02-16 16:10:56 -0800

    sfb/store; #get_or_set

--- lib/sfb/store.rb
+++ lib/sfb/store.rb
@@ -9,12 +9,18 @@ class Sfb::Store
     @cache = {}
   end
 
+  def transaction(read_only: false)
+    @store.transaction(read_only) do
+      yield
+    end
+  end
+
   def [](k)
     if @cache.key?(k)
       @cache[k]
     else
       v = nil
-      @store.transaction(read_only = true) do
+      transaction(read_only: true) do
         v = @store[k]
       end
       @cache[k] = v
@@ -22,9 +28,25 @@ class Sfb::Store
   end
 
   def []=(k, v)
-    @store.transaction do
+    transaction do
       @store[k] = v
       @cache.delete(k)
+    end
+  end
+
+  def key?(k)
+    r = @cache.key?(k)
+    return(r) if r
+    transaction(read_only: true) do
+      @store.root?(k)
+    end
+  end
+
+  def get_or_set(k)
+    if key?(k)
+      self[k]
+    else
+      self[k] = yield
     end
   end
 end
