4079ead12f70b6a3f0a68d1c5429f165a3f01471
stoodfarback <stoodfarback@gmail.com>
2024-05-15 14:54:53 -0700

    tweak requires

--- lib/sfb/outside_gems.rb
+++ lib/sfb/outside_gems.rb
@@ -8,10 +8,9 @@ require("active_support/core_ext/enumerable")
 require("active_support/core_ext/string/inflections")
 require("active_support/core_ext/array/wrap")
 require("active_support/core_ext/string/starts_ends_with")
-require("pry")
-require("redis")
 
 autoload(:JSON, "json")
+autoload(:Redis, "redis")
 autoload(:XXhash, "xxhash")
 autoload(:SecureRandom, "securerandom")
 module Base32; autoload :Crockford, 'base32/crockford'; end
