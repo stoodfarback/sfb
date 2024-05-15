8ae6ec5fdece2674f2d1da55f0443f4689911921
stoodfarback <stoodfarback@gmail.com>
2024-05-14 17:32:26 -0700

    update requires

--- lib/sfb/outside_gems.rb
+++ lib/sfb/outside_gems.rb
@@ -3,6 +3,7 @@
 require("active_support/core_ext/object/try")
 require("active_support/core_ext/object/blank")
 require("active_support/core_ext/string/access")
+require("active_support/core_ext/array/grouping")
 require("json")
 require("pry")
 require("xxhash")
