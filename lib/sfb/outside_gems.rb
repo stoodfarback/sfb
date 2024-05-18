2aaa814c4f547ae70981d37813b32e043fee8963
stoodfarback <stoodfarback@gmail.com>
2024-05-18 09:02:18 -0700

    tweak default requires

--- lib/sfb/outside_gems.rb
+++ lib/sfb/outside_gems.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require("tmpdir")
+
 require("active_support/core_ext/object/try")
 require("active_support/core_ext/object/blank")
 require("active_support/core_ext/module/delegation")
