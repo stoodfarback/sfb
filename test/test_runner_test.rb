bd03b07ea127ac89345633afe002a559be4dff3b
stoodfarback <stoodfarback@gmail.com>
2026-05-03 13:54:58 -0700

    TestRunner test: add snapshot asserts

--- test/test_runner_test.rb
+++ test/test_runner_test.rb
@@ -13,11 +13,21 @@ class TestRunnerTest < Minitest::Test
     [stdout + stderr, status]
   end
 
+  def assert_snapshot_clean(output)
+    cleaned = output.
+      gsub(/--seed \d+/, "--seed <SEED>").
+      gsub(/Finished in \d+\.\d+s/, "Finished in <TIME>s").
+      gsub(/[\d.]+ runs\/s, [\d.]+ assertions\/s/, "<RATE> runs/s, <RATE> assertions/s").
+      gsub(/= \d+\.\d+ s =/, "= <TIME> s =")
+    assert_snapshot(cleaned)
+  end
+
   def test_all_tests_run
     output, status = run_fixture
     assert(status.success?)
     assert_match(/4 runs/, output)
     assert_match(/0 failures/, output)
+    assert_snapshot_clean(output)
   end
 
   def test_pattern_filter
@@ -25,19 +35,22 @@ class TestRunnerTest < Minitest::Test
     assert(status.success?)
     assert_match(/2 runs/, output)
     assert_match(/0 failures/, output)
+    assert_snapshot_clean(output)
   end
 
   def test_verbose_flag
-    output, status = run_fixture("--verbose")
+    output, status = run_fixture("--verbose", "--seed", "1")
     assert(status.success?)
     assert_match(/AlphaTest#test_one/, output)
     assert_match(/BetaTest#test_first/, output)
+    assert_snapshot_clean(output)
   end
 
   def test_seed_flag
     output, status = run_fixture("--seed", "12345")
     assert(status.success?)
     assert_match(/--seed 12345/, output)
+    assert_snapshot_clean(output)
   end
 
   def test_list_flag
@@ -47,14 +60,16 @@ class TestRunnerTest < Minitest::Test
     assert_match(/AlphaTest/, output)
     assert_match(/test_one/, output)
     refute_match(/runs/, output)
+    assert_snapshot_clean(output)
   end
 
   def test_pattern_with_verbose
-    output, status = run_fixture("alpha", "--verbose")
+    output, status = run_fixture("alpha", "--verbose", "--seed", "1")
     assert(status.success?)
     assert_match(/AlphaTest#test_one/, output)
     refute_match(/BetaTest/, output)
     assert_match(/2 runs/, output)
+    assert_snapshot_clean(output)
   end
 
   def test_no_match
@@ -64,6 +79,7 @@ class TestRunnerTest < Minitest::Test
     assert_match(/Available tests \(first 3\):/, output)
     assert_match(%r{test/alpha_test\.rb AlphaTest#test_two}, output)
     assert_match(%r{Usage: bin/test}, output)
+    assert_snapshot_clean(output)
   end
 
   def test_no_tests_in_project
@@ -76,6 +92,7 @@ class TestRunnerTest < Minitest::Test
     assert_match(%r{No tests found in test/fixture/empty_dir/\*_test\.rb}, output)
     refute_match(/No tests matched/, output)
     refute_match(%r{Usage: bin/test}, output)
+    assert_snapshot_clean(output)
   end
 
   def test_multiple_patterns_default_or
@@ -83,6 +100,7 @@ class TestRunnerTest < Minitest::Test
     output, status = run_fixture("alpha", "beta")
     assert(status.success?)
     assert_match(/4 runs/, output)
+    assert_snapshot_clean(output)
   end
 
   def test_match_all
@@ -90,11 +108,13 @@ class TestRunnerTest < Minitest::Test
     output, status = run_fixture("--match-all", "alpha", "one")
     assert(status.success?)
     assert_match(/1 runs/, output)
+    assert_snapshot_clean(output)
   end
 
   def test_match_all_list
     output, status = run_fixture("--match-all", "--list", "alpha", "one")
     assert(status.success?)
     assert_match(/1 test\(s\)/, output)
+    assert_snapshot_clean(output)
   end
 end
