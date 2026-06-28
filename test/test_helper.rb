92f65c22d54c330c9d2d762ef515d89c9b1bb176
stoodfarback <stoodfarback@gmail.com>
2026-06-28 12:40:55 -0700

    trivial

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,5 +3,7 @@
 require_relative("../lib/sfb")
 require("minitest/autorun")
 
+# see `unote sfb-test-snapshot` for notes on assert_snapshot
 Sfb::Test::Snapshot.setup!
+
 Sfb::Test::OutputCapture.setup!
